2c0622d3e0a6a32315b81379b8d4d2bd814d97ac
PhucNH <phucnh.dev@gmail.com>
2020-10-20 11:02:29 +0700

    first commit

---
+++ src/buildList.js
@@ -0,0 +1,39 @@
+const { version } = require('../package.json');
+const mainnet = require('./tokens/mainnet.json');
+// const ropsten = require('./tokens/ropsten.json');
+// const rinkeby = require('./tokens/rinkeby.json');
+// const goerli = require('./tokens/goerli.json');
+// const kovan = require('./tokens/kovan.json');
+
+module.exports = function buildList() {
+  const parsed = version.split('.');
+  return {
+    'name': 'LuaSwap Default List',
+    'timestamp': (new Date().toISOString()),
+    'version': {
+      'major': +parsed[ 0 ],
+      'minor': +parsed[ 1 ],
+      'patch': +parsed[ 2 ]
+    },
+    'tags': {},
+    'logoURI': 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x05D3606d5c81EB9b7B18530995eC9B29da05FaBa/logo.png',
+    'keywords': [
+      'luaswap',
+      'default'
+    ],
+    tokens: [
+      ...mainnet,
+      // ...ropsten,
+      // ...goerli,
+      // ...kovan,
+      // ...rinkeby
+    ]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      })
+  };
+};
